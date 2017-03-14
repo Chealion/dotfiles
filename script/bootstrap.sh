fb96c68f126251d20fcadf9cb164f9fd22fe6c4d
Micheal Jones <micheal@mcjones.ca>
2017-03-14 00:08:19 -0600

    bootstrap for strap.sh

---
+++ script/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Altered version of https://github.com/mathiasbynens/dotfiles/blob/master/bootstrap.sh
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms .. ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
