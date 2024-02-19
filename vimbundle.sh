ee0809eb70a4f9b16c06b7ee59bf6a2f756e30cb
Micheal Jones <micheal@mcjones.ca>
2024-02-18 22:53:38 -0700

    Finally get around to moving from syntastic to ale

--- vimbundle.sh
+++ vimbundle.sh
@@ -7,7 +7,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/arcticicestudio/nord-vim
-git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/dense-analysis/ale ~/.vim/bundle/ale
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 
 cd ~/.vim/colors
